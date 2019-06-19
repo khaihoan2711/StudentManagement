9ade11f5a1add1cf641157cd9c08fac44425d878
LE KHAI HOAN <khaihoan2711@gmail.com>
2019-06-19 10:13:36 +0700

    Commit view Students and view Lecturers

---
+++ StudentManagement_ASP.NET-MCV5/Controllers/LecturersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentManagement_ASP.NET_MCV5.Models;
+
+namespace StudentManagement_ASP.NET_MCV5.Controllers
+{
+    public class LecturersController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Lecturers
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Lecturers.ToListAsync());
+        }
+
+        //// GET: Lecturers/Details/5
+        //public async Task<ActionResult> Details(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Lecturer lecturer = await db.Users.FindAsync(id);
+        //    if (lecturer == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(lecturer);
+        //}
+
+        //// GET: Lecturers/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.FacultyId = new SelectList(db.Faculties, "Id", "Name");
+        //    return View();
+        //}
+
+        //// POST: Lecturers/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Create([Bind(Include = "Id,BirthDay,Address,FirstName,LastName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,LecturerCode,HireDate,FacultyId,IsDeleted")] Lecturer lecturer)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Users.Add(lecturer);
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.FacultyId = new SelectList(db.Faculties, "Id", "Name", lecturer.FacultyId);
+        //    return View(lecturer);
+        //}
+
+        //// GET: Lecturers/Edit/5
+        //public async Task<ActionResult> Edit(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Lecturer lecturer = await db.Users.FindAsync(id);
+        //    if (lecturer == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.FacultyId = new SelectList(db.Faculties, "Id", "Name", lecturer.FacultyId);
+        //    return View(lecturer);
+        //}
+
+        //// POST: Lecturers/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Edit([Bind(Include = "Id,BirthDay,Address,FirstName,LastName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,LecturerCode,HireDate,FacultyId,IsDeleted")] Lecturer lecturer)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(lecturer).State = EntityState.Modified;
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.FacultyId = new SelectList(db.Faculties, "Id", "Name", lecturer.FacultyId);
+        //    return View(lecturer);
+        //}
+
+        //// GET: Lecturers/Delete/5
+        //public async Task<ActionResult> Delete(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Lecturer lecturer = await db.Users.FindAsync(id);
+        //    if (lecturer == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(lecturer);
+        //}
+
+        //// POST: Lecturers/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> DeleteConfirmed(string id)
+        //{
+        //    Lecturer lecturer = await db.Users.FindAsync(id);
+        //    db.Users.Remove(lecturer);
+        //    await db.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StudentManagement_ASP.NET-MCV5/Controllers/StudentsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentManagement_ASP.NET_MCV5.Models;
+
+namespace StudentManagement_ASP.NET_MCV5.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Students
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Students.ToListAsync());
+        }
+
+        //// GET: Students/Details/5
+        //public async Task<ActionResult> Details(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Student student = await db.Users.FindAsync(id);
+        //    if (student == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(student);
+        //}
+
+        //// GET: Students/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Students/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Create([Bind(Include = "Id,BirthDay,Address,FirstName,LastName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,StudentCode,EnrollmentDate")] Student student)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Users.Add(student);
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(student);
+        //}
+
+        //// GET: Students/Edit/5
+        //public async Task<ActionResult> Edit(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Student student = await db.Users.FindAsync(id);
+        //    if (student == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(student);
+        //}
+
+        //// POST: Students/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Edit([Bind(Include = "Id,BirthDay,Address,FirstName,LastName,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName,StudentCode,EnrollmentDate")] Student student)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(student).State = EntityState.Modified;
+        //        await db.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(student);
+        //}
+
+        //// GET: Students/Delete/5
+        //public async Task<ActionResult> Delete(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Student student = await db.Users.FindAsync(id);
+        //    if (student == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(student);
+        //}
+
+        //// POST: Students/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> DeleteConfirmed(string id)
+        //{
+        //    Student student = await db.Users.FindAsync(id);
+        //    db.Users.Remove(student);
+        //    await db.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
